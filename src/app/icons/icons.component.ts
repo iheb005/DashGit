e08958d1764a7a95441ec6db746200ec621832ac
iheb <ihebelfekih@gmail.com>
2021-04-24 11:54:03 +0100

    commit_dashboard

--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, ReactiveFormsModule } from '@angular/forms';
 import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 export class User {
@@ -23,21 +23,26 @@ export class User {
 export class IconsComponent implements OnInit {
 users: User[];
 public closeResult: string;
-//editForm: FormGroup;
+editForm: FormGroup;
+//stat: string;
+ private deleteID: number;
+  
 
   constructor(private httpclient: HttpClient,
     private modalService: NgbModal,
-   /*private fb:FormBuilder*/) { }
+    private fb:FormBuilder) { }
  
   ngOnInit() {
     this.getUsers();
-   /* this.editForm = this.fb.group({
+    this.editForm = this.fb.group({
       id: [''],
       firstname: [''],
       lastname: [''],
       email: [''],
       active: ['']
-    } );*/
+    
+    });
+  
   }
 
   getUsers(){
@@ -73,6 +78,9 @@ public closeResult: string;
     this.modalService.dismissAll(); //dismiss the modal
   }
 
+  /*onDelete(){
+    this.httpclient.delete('http:localhost:8001/:id').subscribe(()=> this.stat='delete successful');
+  }*/
   openDetails(targetModal, user: User) {
     this.modalService.open(targetModal, {
      centered: true,
@@ -82,21 +90,51 @@ public closeResult: string;
     document.getElementById('fname').setAttribute('value', user.firstname);
     document.getElementById('lname').setAttribute('value', user.lastname);
     document.getElementById('email2').setAttribute('value', user.email);
-    //document.getElementById('status').setAttribute('value', user.active);
+   // document.getElementById('status').setAttribute('value', user.active);
  }
 
-/* openEdit(targetModal,user:User){
-   this.modalService.open(targetModal, {
-     backdrop:'static',
-     size:'lg'
-   });
-   this.editForm.patchValue( {
-    id: user.id, 
-    firstname: user.firstname,
-    lastname: user.lastname,
-    email: user.email,
-    active: user.active
-  });
- }*/
+
+ openEdit(targetModal, user: User) {
+  this.modalService.open(targetModal, {
+   centered: true,
+   backdrop: 'static',
+   size: 'lg'
+ });
+ this.editForm.patchValue({
+   id:user.id,
+   firstname:user.firstname,
+   lastname: user.lastname,
+   email: user.email,
+   active: user.active
+ });
+}
+ 
+onSave() {
+  const editURL = 'http://localhost:8001/updateuser/' + this.editForm.value.id;
+  console.log(this.editForm.value);
+  this.httpclient.put(editURL, this.editForm.value)
+    .subscribe((results) => {
+      this.ngOnInit();
+      this.modalService.dismissAll();
+    });
+}
+
+openDelete(targetModal, user: User) {
+  this.deleteID = user.id;
+  this.modalService.open(targetModal, {
+   centered: true,
+   backdrop: 'static',
+   size: 'lg'
+ });
+}
+
+onDelete() {
+  const deleteURL = 'http://localhost:8001/' + this.deleteID ;
+  this.httpclient.delete(deleteURL)
+    .subscribe((results) => {
+      this.ngOnInit();
+      this.modalService.dismissAll();
+    });
+}
 
 }
