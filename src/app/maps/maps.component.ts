6a238c891ea4765bd4c5ebfbf37eef1922c216f3
iheb <ihebelfekih@gmail.com>
2021-04-29 14:56:09 +0100

    struct_crud_commit2

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgForm } from '@angular/forms';
 
-declare const google: any;
 
-interface Marker {
-lat: number;
-lng: number;
-label?: string;
-draggable?: boolean;
+export class Structure{
+    constructor(
+        public id_struct: number,
+        public nom : string,
+        public mail: string
+    ){}
 }
+
+
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
@@ -15,11 +20,61 @@ draggable?: boolean;
 })
 export class MapsComponent implements OnInit {
 
-  constructor() { }
+    structure : Structure[];
+    closeResult: string;
+
+  constructor( private httpclient:HttpClient,
+    private modalService: NgbModal
+     ) { }
 
   ngOnInit() {
+    this.getStructures();
+  }
 
-    var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
+  getStructures(){
+    this.httpclient.get<any>('http://localhost:8001/structures').subscribe(response =>{
+        console.log(response);
+        this.structure = response;
+    })
+}
+
+    open(content){
+        this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+            this.closeResult = `closed with ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+
+   private getDismissReason(reason: any): string{
+if(reason === ModalDismissReasons.ESC) {
+    return 'by pressing ESC';
+} else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+    return 'by clicking on a backdrop';
+} else{
+    return ` with : ${reason}`;
+}
+    }
+
+    onSubmit(f:NgForm){
+        const url = 'http://localhost:8001/addstruct';
+        this.httpclient.post(url,f.value).subscribe((result) =>{
+            this.ngOnInit(); //refresh table
+        });
+        this.modalService.dismissAll(); //dismiss modal
+    }
+
+openDetails(targetModal, structure: Structure){
+this.modalService.open(targetModal, {
+    centered: true,
+    backdrop: 'static',
+    size: 'lg'
+});
+document.getElementById('nom').setAttribute('value',structure.nom);
+document.getElementById('mail').setAttribute('value',structure.mail);
+}
+
+    /*    var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
     var mapOptions = {
         zoom: 13,
         center: myLatlng,
@@ -111,15 +166,16 @@ export class MapsComponent implements OnInit {
         }]
 
     };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+*/
+    //  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    var marker = new google.maps.Marker({
+   /* var marker = new google.maps.Marker({
         position: myLatlng,
         title: "Hello World!"
     });
-
+*/
     // To add the marker to the map, call setMap();
-    marker.setMap(map);
-  }
+  //  marker.setMap(map);
+  
 
 }
