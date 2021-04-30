6a7235467d1ffba195807f6861859e14fa29b4a7
iheb <ihebelfekih@gmail.com>
2021-04-30 14:50:15 +0100

    commit30_04_21

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormBuilder } from '@angular/forms';
+
 
 
 export class Structure{
@@ -22,13 +23,19 @@ export class MapsComponent implements OnInit {
 
     structure : Structure[];
     closeResult: string;
+    editForm: FormGroup;
 
   constructor( private httpclient:HttpClient,
-    private modalService: NgbModal
-     ) { }
+    private modalService: NgbModal,
+    private formb: FormBuilder) { }
 
   ngOnInit() {
     this.getStructures();
+    this.editForm = this.formb.group({
+        id_struct :[''],
+        nom : [''],
+        mail : ['']
+    });
   }
 
   getStructures(){
@@ -70,112 +77,22 @@ this.modalService.open(targetModal, {
     backdrop: 'static',
     size: 'lg'
 });
-document.getElementById('nom').setAttribute('value',structure.nom);
-document.getElementById('mail').setAttribute('value',structure.mail);
+document.getElementById('name').setAttribute('value',structure.nom);
+document.getElementById('mail1').setAttribute('value',structure.mail);
 }
 
-    /*    var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
-    var mapOptions = {
-        zoom: 13,
-        center: myLatlng,
-        scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-        styles: [{
-            "featureType": "water",
-            "stylers": [{
-                "saturation": 43
-            }, {
-                "lightness": -11
-            }, {
-                "hue": "#0088ff"
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "hue": "#ff0000"
-            }, {
-                "saturation": -100
-            }, {
-                "lightness": 99
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#808080"
-            }, {
-                "lightness": 54
-            }]
-        }, {
-            "featureType": "landscape.man_made",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#ece2d9"
-            }]
-        }, {
-            "featureType": "poi.park",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#ccdca1"
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "labels.text.fill",
-            "stylers": [{
-                "color": "#767676"
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "labels.text.stroke",
-            "stylers": [{
-                "color": "#ffffff"
-            }]
-        }, {
-            "featureType": "poi",
-            "stylers": [{
-                "visibility": "off"
-            }]
-        }, {
-            "featureType": "landscape.natural",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "visibility": "on"
-            }, {
-                "color": "#b8cb93"
-            }]
-        }, {
-            "featureType": "poi.park",
-            "stylers": [{
-                "visibility": "on"
-            }]
-        }, {
-            "featureType": "poi.sports_complex",
-            "stylers": [{
-                "visibility": "on"
-            }]
-        }, {
-            "featureType": "poi.medical",
-            "stylers": [{
-                "visibility": "on"
-            }]
-        }, {
-            "featureType": "poi.business",
-            "stylers": [{
-                "visibility": "simplified"
-            }]
-        }]
+openEdit(targetModal, structure:Structure){
+this.modalService.open(targetModal, {
+    centered:true,
+    backdrop: 'static',
+    size: 'lg'
+});
+this.editForm.patchValue({
+    id_struct: structure.id_struct,
+    nom: structure.nom,
+    mail: structure.mail
+});
+}
 
-    };
-*/
-    //  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-   /* var marker = new google.maps.Marker({
-        position: myLatlng,
-        title: "Hello World!"
-    });
-*/
-    // To add the marker to the map, call setMap();
-  //  marker.setMap(map);
-  
 
 }
